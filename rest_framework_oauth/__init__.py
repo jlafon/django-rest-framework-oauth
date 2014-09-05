c012c4d968bf9dbccc350bac878488670587a44e
Jharrod LaFon <jlafon@eyesopen.com>
2014-09-05 10:43:18 -0700

    Module __init__

--- rest_framework_oauth/__init__.py
+++ rest_framework_oauth/__init__.py
@@ -1,3 +1,11 @@
 """
 OAuth Support for Django REST Framework
 """
+__title__ = 'Django REST framework OAuth'
+__version__ = '0.1'
+__author__ = 'Jharrod LaFon'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Jharrod LaFon'
+
+# Version synonym
+VERSION = __version__
